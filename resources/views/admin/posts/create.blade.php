7efa9aae60a2ce40b74b4f52653f69ac18f6d67f
Chris Foster <wordpressninja@me.com>
2017-10-10 00:15:00 -0500

    Added HomeController, PostController, Auth Controller/Views, layouts app.blade template and menus.  Created the create post view added the form fields and dumping data onto view

---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('content')
+	 <div class="panel panel-default">
+                <div class="panel-heading">Create New Post</div>
+
+                <div class="panel-body">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                  <form action="/post/store" method="post" >
+                  	{{ csrf_field() }}
+             
+                  		<fieldset class="form-group">
+                  			<label for="title">Title</label>
+                  			<input type="text" class="form-control" id="title" placeholder="Post Title" name="title">
+                  		</fieldset>
+                  		<fieldset class="form-group">
+                  			<label for="content">Content</label>
+                  			<textarea class="form-control" id="content" cols="10" rows="10" placeholder="Post Text Area" name="content"></textarea>
+                  		</fieldset>                  		
+                  		<fieldset class="form-group">
+                  			<label for="featured">Featured Image</label>
+                  			<input type="file" class="form-control" id="featured" name="featured">
+                  		</fieldset>
+                   		<fieldset class="form-group">
+                  			<div class="text-center">
+                  				<button type="submit" class="btn btn-success">Create Post</button>
+                  			</div>
+                  		</fieldset>                 	
+                  </form>
+                </div>
+    </div>
+@stop
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,17 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/post/create', [
+	'uses' => 'PostsController@create',
+	'as' => 'post.create'
+]);
+
+Route::post('/post/store', [
+	'uses' => 'PostsController@store',
+	'as' => 'post.store'
+]);
